d1625b4ea13f6cdb5f163527df680b0c0bdb0791
Luigi MASTROSERIO <luigi.mastroserio@gmail.com>
2017-07-12 21:16:08 +0200

    peakycake starts here

---
+++ src/main/java/org/kg6682/peakycake/PeakycakeApplication.java
@@ -0,0 +1,12 @@
+package org.kg6682.peakycake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeakycakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeakycakeApplication.class, args);
+	}
+}
